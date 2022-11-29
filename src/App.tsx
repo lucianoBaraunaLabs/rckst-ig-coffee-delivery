05bf43dd7519166adf3e5f42372c053b7fb166da
Luciano Baraúna <baraunaluciano@gmail.com>
2022-11-29 08:34:15 -0300

    chore: configure theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iure
+        repudiandae provident, doloremque eius reprehenderit at vero
+        consequuntur quibusdam nemo dolores a pariatur corporis voluptas
+        adipisci porro nostrum dignissimos velit?
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-export default App
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,111 @@
+export const defaultTheme = {
+  white: '#FFF',
+  black: '#000',
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-900': '#C47F17',
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+  colorBase: {
+    background: '#FAFAFA',
+    title: '#272221',
+    subtitle: '#403937',
+    text: '#574F4D',
+    label: '#8D8686',
+    hover: '#D7D5D5',
+    button: '#E6E5E5',
+    input: '#EDEDED',
+    card: '#F3F2F2',
+  },
+  fontBase: '"Roboto", sans-serif',
+  fontHeader: '"Baloo 2", cursive',
+  fontSize: {
+    10: '0.625rem',
+    12: '0.75rem',
+    14: '0.875rem',
+    base: '1rem',
+    18: '1.125rem',
+    20: '1.25rem',
+    24: '1.5rem',
+    32: '2rem',
+    48: '3rem',
+  },
+  fontWeight: {
+    bold: 700,
+    regular: 400,
+    extraBold: 800,
+  },
+  spacing: {
+    /** 1px */
+    px: '1px',
+    /** 0px */
+    0: '0px',
+    /** 2px */
+    0.5: '0.125rem',
+    /** 4px */
+    1: '0.25rem',
+    /** 6px */
+    1.5: '0.375rem',
+    /** 8px */
+    2: '0.5rem',
+    /** 10px */
+    2.5: '0.625rem',
+    /** 12px */
+    3: '0.75rem',
+    /** 14px */
+    3.5: '0.875rem',
+    /** 16px */
+    4: '1rem',
+    /** 20px */
+    5: '1.25rem',
+    /** 24px */
+    6: '1.5rem',
+    /** 28px */
+    7: '1.75rem',
+    /** 32px */
+    8: '2rem',
+    /** 36px */
+    9: '2.25rem',
+    /** 40px */
+    10: '2.5rem',
+    /** 44px */
+    11: '2.75rem',
+    /** 48px */
+    12: '3rem',
+    /** 56px */
+    14: '3.5rem',
+    /** 64px */
+    16: '4rem',
+    /** 80px */
+    20: '5rem',
+    /** 96px */
+    24: '6rem',
+    /** 112px */
+    28: '7rem',
+    /** 128px */
+    32: '8rem',
+    /** 144px */
+    36: '9rem',
+    /** 160px */
+    40: '10rem',
+    /** 176px */
+    44: '11rem',
+    /** 192px */
+    48: '12rem',
+    /** 208px */
+    52: '13rem',
+    /** 224px */
+    56: '14rem',
+    /** 240px */
+    60: '15rem',
+    /** 256px */
+    64: '16rem',
+    /** 288px */
+    72: '18rem',
+    /** 320px */
+    80: '20rem',
+    /** 384px */
+    96: '24rem',
+  },
+}
