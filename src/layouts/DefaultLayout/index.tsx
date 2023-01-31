3d2a3709816674be61aacdeba1e7cdc1cbfd04ea
Luciano Baraúna <baraunaluciano@gmail.com>
2023-01-30 23:45:44 -0300

    feat(Header): implemented quantity itens

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { CoffeeContext } from '../../contexts/CoffeeContext'
 import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
+  const { infoCart } = useContext(CoffeeContext)
   return (
     <LayoutContainer>
-      <Header quantityItensCart={10} />
+      <Header quantityItensCart={infoCart.quantity} />
       <Outlet />
     </LayoutContainer>
   )
