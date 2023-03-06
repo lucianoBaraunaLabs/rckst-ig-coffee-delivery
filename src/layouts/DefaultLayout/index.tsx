92ac4f74552bb422210a14cd40a10f089747f19b
Luciano Baraúna <baraunaluciano@gmail.com>
2023-03-06 12:56:39 -0300

    style(layout): change imports to organize

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -2,14 +2,14 @@ import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { CoffeeContext } from '../../contexts/CoffeeContext'
-import { LayoutContainer } from './styles'
+import * as S from './styles'
 
 export function DefaultLayout() {
   const { infoCart } = useContext(CoffeeContext)
   return (
-    <LayoutContainer>
+    <S.LayoutContainer>
       <Header quantityItensCart={infoCart.quantity} />
       <Outlet />
-    </LayoutContainer>
+    </S.LayoutContainer>
   )
 }
