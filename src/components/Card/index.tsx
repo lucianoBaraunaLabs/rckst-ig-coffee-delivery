cb08bbc72251b6284f9e133b13a778e0895af401
Luciano Baraúna <baraunaluciano@gmail.com>
2023-03-06 14:01:35 -0300

    style(card): change imports to organize code

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,32 +1,18 @@
 import { ShoppingCartSimple, Trash } from 'phosphor-react'
 import { useContext, useState } from 'react'
-import {
-  CardContainer,
-  ImageContainer,
-  InfoContainer,
-  List,
-  Title,
-  Text,
-  Price,
-  ButtonCart,
-  Footer,
-  Controls,
-  CardPropStyles,
-  ButtonRemove,
-} from './styles'
+import * as S from './styles'
 
 import { InputQuantity } from '../InputQuantity'
 
 import { Coffee, CoffeeContext, CoffeId } from '../../contexts/CoffeeContext'
 
-interface CardProps extends CardPropStyles {
+interface CardProps extends S.CardPropStyles {
   coffee: Coffee
 }
 
 export function Card({ variation = 'card', coffee }: CardProps) {
   const { addCoffee, removeCoffee } = useContext(CoffeeContext)
-  const quantityProd = coffee.quantity || 1
-  const [quantity, setQuantity] = useState(quantityProd)
+  const [quantityProd, setQuantityProd] = useState(coffee.quantity || 1)
   const isVariationCard = variation !== 'row'
   const isVariationRow = variation === 'row'
   const currencyValue = new Intl.NumberFormat('pt-br', {
@@ -34,15 +20,15 @@ export function Card({ variation = 'card', coffee }: CardProps) {
   }).format(coffee.price)
 
   function handleDecrement() {
-    setQuantity((state) => state - 1)
+    setQuantityProd((state) => state - 1)
   }
 
   function handleIncrement() {
-    setQuantity((state) => state + 1)
+    setQuantityProd((state) => state + 1)
   }
 
   function handleAddProduct() {
-    const product = { ...coffee, quantity }
+    const product = { ...coffee, quantity: quantityProd }
     addCoffee(product)
   }
 
@@ -51,71 +37,71 @@ export function Card({ variation = 'card', coffee }: CardProps) {
   }
 
   function handleChangeDecrement() {
-    setQuantity((state) => state - 1)
+    setQuantityProd((state) => state - 1)
     handleAddProduct()
   }
 
   function handleChangeIncrement() {
-    setQuantity((state) => state + 1)
+    setQuantityProd((state) => state + 1)
     handleAddProduct()
   }
 
   return (
-    <CardContainer variation={variation}>
-      <ImageContainer>
+    <S.CardContainer variation={variation}>
+      <S.ImageContainer>
         <img src={`./src/assets/produtos/${coffee.img}`} alt="" />
-      </ImageContainer>
+      </S.ImageContainer>
 
-      <InfoContainer>
+      <S.InfoContainer>
         {isVariationCard && (
-          <List>
+          <S.List>
             {coffee.tags.map((tagValue) => {
               return <li key={`${coffee.id}_${tagValue}`}>{tagValue}</li>
             })}
-          </List>
+          </S.List>
         )}
 
-        <Title>{coffee.name}</Title>
+        <S.Title>{coffee.name}</S.Title>
 
-        {isVariationCard && <Text>{coffee.description}</Text>}
+        {isVariationCard && <S.Text>{coffee.description}</S.Text>}
 
         {isVariationRow && (
-          <Controls>
+          <S.Controls>
             <InputQuantity
               onDecrement={handleChangeDecrement}
               onIncrement={handleChangeIncrement}
-              quantity={quantity}
+              quantity={quantityProd}
             />
-            <ButtonRemove
+            <S.ButtonRemove
               variation="simple"
               onClick={() => handleRemoveProduct(coffee.id)}
             >
               <Trash size="1rem" /> REMOVER
-            </ButtonRemove>
-          </Controls>
+            </S.ButtonRemove>
+          </S.Controls>
         )}
-      </InfoContainer>
+      </S.InfoContainer>
 
-      <Footer>
-        <Price>
+      <S.Footer>
+        <S.Price>
           <span>R$</span> {currencyValue}
-        </Price>
+        </S.Price>
         {isVariationCard && (
-          <Controls as="div">
+          <S.Controls as="div">
             <InputQuantity
               onDecrement={handleDecrement}
               onIncrement={handleIncrement}
-              quantity={quantity}
+              quantity={quantityProd}
             />
-            <ButtonCart
+            <S.ButtonCart
               onClick={handleAddProduct}
               title="Adicionar ao carrinho"
             >
               <ShoppingCartSimple weight="fill" />
-            </ButtonCart>
-          </Controls>
+            </S.ButtonCart>
+          </S.Controls>
         )}
-      </Footer>
-    </CardContainer>
+      </S.Footer>
+    </S.CardContainer>
   )
 }
